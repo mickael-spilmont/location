4794b9882465d7827fdacfb3c0c4bb004c1a2223
Mickael-Spilmont <mickael.spilmont.fr@gmail.fr>
2017-12-28 16:30:56 +0100

    Reprise à zero de la base de donnée

--- BaseDeDonnee.java
+++ BaseDeDonnee.java
@@ -2,58 +2,25 @@ import java.util.Scanner;
 class BaseDeDonnee{
     Scanner Scan = new Scanner(System.in);
 
-    // base locataires
-    private int vLocId []=new int [500];//numero id d'adherent
-    private String vLocNo []=new String[500];//nom d'adherent
-    private String vLocAd []=new String[500];//adresse d'adherent
-    private int vLocBi [][]=new int [500][5];//id de biens loués (5 par adherent)
+//    base locataires
+    int idLocataire []=new int [500];//numero id du locataire
+    String nomLocataire []=new String[500];//nom du locataire
+    String adresseLocataire []=new String[500];//adresse du locataire
+    int idBienLocataire [][]=new int [500][5];//id de bien loués (5 par locataire)
 
-    // base type
-    private int vTyBiId []=new int [20];//id type de biens
-    private String vTyBiNo []=new String [20];//nom type de biens
+//    base type
+    int idType []=new int [20];//id type de biens
+    String nomType []=new String [20];//nom type de biens
 
-    // base biens
-    private int vBiId []=new int [100];//numero id des biens
-    private int vBiTy []=new int [100];//numero id des types de biens
-    private String vBiDes []=new String [100];//description des biens
-    private int vBiLoc []=new int [100];//numero id des locataires biens
+//    base biens
+    int idBien []=new int [100];//numero id des bien
+    int idTypeBien []=new int [100];//numero id des types de bien
+    String adresseBien []=new String [100];//description des bien
+    int idLocataireBien []=new int [100];//numero id des locataires bien
+    double loyerBien[] = new double [100];
 
-    // compteurID
-    private int cIdLocataire = 0;
-    private int cIdType = 0;
-    private int cIdBiens = 0;
-    
-    // commandes
-     void supprimerLoc(int n){
-        vLocId[n]=vLocId[cIdLocataire];
-        vLocId[cIdLocataire]=0;
-        vLocNo[n]=vLocNo[cIdLocataire];
-        vLocNo[cIdLocataire]=0;
-        vLocAd[n]=vLocAd[cIdLocataire];
-        vLocAd[cIdLocataire]=0;
-        vLocBi[n]=vLocBi[cIdLocataire];
-        vLocBi[cIdLocataire]=0;         
-        cIdLocataire=cIdLocataire-1;
-    }
-    void supprimerBien(int n){
-        vBiId[n]=vBiId[cIdBiens];
-        vBiId[cIdBiens]=0;
-        vBiTy[n]=vBiTy[cIdBiens];
-        vBiTy[cIdBiens]=0;
-        vBiDes[n]=vBiDes[cIdBiens];
-        vBiDes[cIdBiens]=0;
-        vBiLoc[n]=vBiLoc[cIdBiens];
-        vBiLoc[cIdBiens]=0;         
-        cIdBiens=cIdBiens-1;
-    }
-    void supprimerTypeBien(int n){
-        vTyBiId[n]=vTyBiId[cIdType];
-        vTyBiId[cIdType]=0;
-        vTyBiNo[n]=vTyBiNo[cIdType];
-        vTyBiNo[cIdType]=0;         
-        cIdType=cIdType-1;
-    }
-    void ajouterLoc(){
-        System.out.print("")
-    }
+//    compteurID
+    int compteurLocataire[] = {0, 0};
+    int compteurType[] = {0, 0};
+    int compteurBien[] = {0, 0};
 }
