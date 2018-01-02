e0a1432f72a711ac590271f8beabf895f1a9e60e
mickael <mickael.spilmont.fr@gmail.com>
2018-01-03 00:10:57 +0100

    Travail sur le fichier GestionFichier : Finallisation de la procédure de sauvegarde/chargement de la base comnplète

--- GestionFichier.java
+++ GestionFichier.java
@@ -22,27 +22,155 @@ class GestionFichier {
     }
 
     /**
-     * Met le fichier de sauvegarde en mémoire tampon pour l'écriture
+     * Méthode qui permet la sauvegarde de la base dans un fichier, les bases sont sauvegardées les une en dessous des
+     * autres dans cet ordre : locataires, type de biens, biens.
+     * Les données sont enregistrées ligne par ligne, par exemple pour la classe locataire on commence par enregistrer
+     * toutes les données relatif au premier locataire puis on passe au second.
+     * Les données sous forme de String sont sauvegardées sur une seule ligne par base, chaque données étant séparée
+     * par un ";".
+     * @param base
+     *  La base de données à enregistrer
+     * @return
+     *  Une phrase qui indique si la sauvegarde à réussie
      * @throws IOException
      */
-    DataOutputStream ecrire() throws IOException{
+    String ecrire(BaseDeDonnees base) throws IOException{
+        String chaineDonnees = null;
         DataOutputStream dos;
 
         dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(cheminFichier)));
 
-        return dos;
+//        Base locataires
+        int nbLocataires = base.compteurLocataire[0];
+
+        dos.writeInt(base.compteurLocataire[0]);
+        dos.writeInt(base.compteurLocataire[1]);
+
+        for (int i = 0 ; i <= nbLocataires ; i++){
+            dos.writeInt(base.idLocataire[i]);
+
+            chaineDonnees = "";
+            for (int j = 0 ; j < 4 ; j++){
+                chaineDonnees += base.donneesLocataire[i][j] + ";";
+            }
+            dos.writeUTF(chaineDonnees);
+
+            for (int k = 0 ; k < 5 ; k++){
+                dos.writeInt(base.idBienLocataire[i][k]);
+            }
+        }
+
+//        Base types
+        int nbTypes = base.compteurType[0];
+
+        dos.writeInt(base.compteurType[0]);
+        dos.writeInt(base.compteurType[1]);
+
+        for (int i = 0 ; i <= nbTypes ; i++){
+            dos.writeInt(base.idType[i]);
+            dos.writeUTF(base.nomType[i]);
+        }
+
+//        Base biens
+        int nbBiens = base.compteurBien[0];
+
+        dos.writeInt(base.compteurBien[0]);
+        dos.writeInt(base.compteurBien[1]);
+
+        for (int i = 0 ; i <= nbBiens ; i++){
+
+            for (int j = 0 ; j < 3 ; j ++){
+                dos.writeInt(base.clesBien[i][j]);
+            }
+
+            chaineDonnees = "";
+            for (int k = 0 ; k < 2 ; k++){
+                chaineDonnees += base.donneesBien[i][k] + ";";
+            }
+            dos.writeUTF(chaineDonnees);
+
+            dos.writeDouble(base.loyerBien[i]);
+        }
+
+        dos.close();
+        return "La sauvegarde est terminée avec succès";
     }
 
+
     /**
-     * Met le fichier de sauvegarde en mémoire tampon pour la lecture
+     * Méthode qui permet de charger les info contenues dans le fichier de sauvegarde vers la base de données.
+     * Les info sont chargées dans le même ordre que celui d'écriture dans le fichier (voir ligne 25)
+     * @param base
+     *  La base de données qui vas recevoir le contenu du fichier
+     * @return
+     *  Une phrase qui indique si le chargement à réussi
      * @throws IOException
      */
-    DataInputStream lire() throws IOException{
+    String lire(BaseDeDonnees base) throws IOException{
+        String chaineDonnees = null;
         DataInputStream dis;
 
         dis = new DataInputStream(new BufferedInputStream(new FileInputStream(cheminFichier)));
 
-        return dis;
+//        Locataire
+        base.compteurLocataire[0] = dis.readInt();
+        base.compteurLocataire[1] = dis.readInt();
+
+        int nbLocataires = base.compteurLocataire[0];
+
+        for (int i = 0 ; i <= nbLocataires ; i++){
+            base.idLocataire[i] = dis.readInt();
+
+//            Lis la chaine de doneesLocataire et la découpe à chaque ";" avec la méthode de classe split qui retourne
+//            un tableau contenant chaque parties
+            chaineDonnees = dis.readUTF();
+            int j = 0;
+            for (String donnee : chaineDonnees.split(";")){
+                base.donneesLocataire[i][j] = donnee;
+                j ++;
+            }
+
+            for (int k = 0 ; k < 5 ; k++){
+                base.idBienLocataire[i][j] = dis.readInt();
+            }
+        }
+
+//        Types
+        base.compteurType[0] = dis.readInt();
+        base.compteurType[1] = dis.readInt();
+
+        int nbTypes = base.compteurType[0];
+
+        for (int i = 0 ; i <= nbTypes ; i++){
+            base.idType[i] = dis.readInt();
+            base.nomType[i] = dis.readUTF();
+        }
+
+//        Biens
+        base.compteurBien[0] = dis.readInt();
+        base.compteurBien[1] = dis.readInt();
+
+        int nbBiens = base.compteurBien[0];
+
+        for (int i = 0 ; i <= nbBiens ; i++){
+
+            for (int j = 0 ; j < 3 ; j++){
+                base.clesBien[i][j] = dis.readInt();
+            }
+
+//            Même procédure que pour donneesLocataire, voir ligne 118
+            chaineDonnees = dis.readUTF();
+            int k = 0;
+            for (String donnee : chaineDonnees.split(";")){
+                base.donneesBien[i][k] = donnee;
+                k ++;
+            }
+
+            base.loyerBien[i] = dis.readDouble();
+        }
+
+        dis.close();
+        return "Base de données chargée avec succès";
     }
 
     /**
@@ -82,21 +210,25 @@ class GestionFichier {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException{
-        GestionFichier objetFichier;
+        GestionFichier objetFichier = new GestionFichier("test");
+        BaseDeDonnees baseA = new BaseDeDonnees();
+        BaseDeDonnees baseB = new BaseDeDonnees();
 
-        objetFichier = new GestionFichier("bidule");
-        objetFichier.creerRepertoireSauvegardes();
-        objetFichier.ecrire();
-        objetFichier.listerFichier();
+//        Ajout de deux locataires dans la base
+        baseA.ajouterLocataire("Nollan", "Victor", "11 rue de la monaie 59000 Lille", "0325684475");
+        baseA.ajouterLocataire("Smith", "Jhon", "13 rue du moulin 59000 Lille", "0325889415");
+        baseA.ajouterLocataire("Norris", "Chuck", "53 Bvd Louis XIV 59000 Lille", "0395787615");
 
-        if (objetFichier.existe()){
-            System.out.println("Le fichier existe");
+//        Sauvegarde de la base
+        objetFichier.ecrire(baseA);
+
+//        Chargement dan sune nouvelle base
+        objetFichier.lire(baseB);
+
+//        Lire baseB
+        for (int i = 0 ; i < 3 ; i++){
+            System.out.println(baseB.afficherLocataire(i));
         }
-        else{
-            System.out.println("Le fichier n'existe pas");
-        }
-
-        objetFichier.lire();
     }
 }
 
